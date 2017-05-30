8d237e603f66576e5dcf6877d5c5cfbaa4ff1246
Leog <leog91@gmail.com>
2017-05-30 02:50:16 -0300

    basic route/state

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,98 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope) { })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+
+
+  .controller('LoginCtrl', function ($scope, $state, $rootScope, userService, apiService, $ionicAuth, $ionicUser) {
+
+
+
+
+    //
+    /*
+        $rootScope.$on('event:social-sign-in-success', function (event, userDetails) {
+          console.log(userDetails);
+    
+          userService.setUser(userDetails);
+          apiService.logIn().then(function (response) {
+            console.log("user exist");
+            apiService.getProfile().then(function (response) {
+              userService.setProfile(response.data);
+            })
+            $window.location.href = '/#/main';
+          },
+            function (error) {
+              console.log("creating user");
+              $window.location.href = '/#/settings';
+            });
+    
+          console.log(userService.getEmail());
+    
+    
+    
+        })
+        $scope.signout = function () {
+          socialLoginService.logout();
+        }
+    
+        $scope.$on('event:social-sign-out-success', function (event, userDetails) {
+          $scope.result = userDetails;
+        })
+    
+        //
+    
+    */
+
+
+
+
+
+
+    $scope.signIn = function () {
+
+      $ionicAuth.login('google');
+     // console.log($ionicUser.social.google.data.email);
+        $state.go('tab.dash');
+
+      /*.then(function (response) {
+        console.log('Sign-In ok');
+        $state.go('tab.dash');
+      }, function (error) {
+        console.log('Sign-In fail');
+      });
+*/
+    };
+
+
+  })
+
+
+
+
+
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,278 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+
+  .service('userService', function () {
+
+    var auth = {};
+
+    var country = "ARS";
+
+    var bundle = {};
+
+    var email = "";
+
+    var name = "";
+
+    var imageUrl = "";
+
+    //m
+    var profile = {};
+
+    var isLogged = false;
+
+
+
+    return {
+
+      setProfile: function (user) {
+        profile = user;
+      },
+
+      getProfile: function () {
+        return profile;
+      },
+
+
+      setUser: function (user) {
+        //var userNameWithoutMail = userService.getEmail().slice(0, userService.getEmail().indexOf("@"));
+        //email = user.email;
+        email = user.email.slice(0, user.email.indexOf("@"));
+        name = user.name;
+        imageUrl = user.imageUrl;
+        isLogged = true;
+      },
+
+
+
+      //removeU
+
+      getEmail: function () {
+        return email;
+      },
+
+      getName: function () {
+        return name;
+      },
+
+      getImage: function () {
+        return imageUrl;
+      },
+
+      setCountry: function (newCountry) {
+        country = newCountry;
+      },
+
+      getCountry: function () {
+        return country;
+      },
+
+
+
+      isLoggedIn: function () {
+        return isLogged;
+      },
+
+      logOut: function () {
+        email = "";
+        name = "";
+        imageUrl = "";
+        isLogged = false;
+      },
+
+    };
+
+  })
+
+
+
+  .service('apiService', function ($http, userService) {
+
+    var auth = {};
+
+    var country = "ARS";
+
+    var bundle = {};
+
+    var currentGroupSize = 0;
+
+    return {
+
+
+      url: function () {
+        return "http://localhost:8080/";
+      },
+
+
+
+      saveItem: function (item, date) {
+        return $http({
+          method: 'get',
+          url: this.url() + "item/add/" + userService.getEmail() + "/" + this.dateUrl(date) + this.itemUrl(item) + "/" + currentGroupSize
+        });
+      },
+
+      updateItem: function (item, date, id) {
+        return $http({
+          method: 'get',
+          url: this.url() + "item/update/" + id + "/" + userService.getEmail() + "/" + this.dateUrl(date) + this.itemUrl(item) + "/" + currentGroupSize
+        });
+      },
+
+      deleteItem: function (id) {
+        return $http({
+          method: 'get',
+          url: this.url() + "item/delete/" + id
+        });
+      },
+
+
+      itemUrl: function (item) {
+        return item.name + "/" + item.ammount + "/" + item.currency + "/" + item.category
+      },
+
+      dateUrl: function (date) {
+        return date.getDate() + "/" + (date.getMonth() + + 1) + "/" + date.getFullYear() + "/";
+      },
+
+
+      getItems: function () {
+        return $http({
+          method: 'get',
+          url: this.url() + "item/user/" + userService.getEmail()
+        });
+      },
+
+      getBetweenDates: function (dateFrom, dateTo) {
+        return $http({
+          method: 'get',
+          url: this.url() + "item/betweendates/" + userService.getEmail() + "/" + this.dateUrl(dateFrom) + this.dateUrl(dateTo)
+        });
+      },
+
+
+      byCategory: function (category) {
+        return $http({
+          method: 'get',
+          url: this.url() + "item/categoryuser/" + userService.getEmail() + "/" + category
+        });
+      },
+
+      getCountry: function () {
+        return country;
+      },
+
+
+      saveSettings: function (countryCode, groupSize) {
+        return $http({
+          method: 'get',
+          url: this.url() + "user/saveSettings/" + userService.getEmail() + "/" + countryCode + "/" + groupSize
+        });
+      },
+
+
+      addRatio: function () {
+        return $http({
+          method: 'post',
+          url: this.url() + "currency/add/20/4/2017/USD/ARS/16"
+        });
+      },
+
+      getProfile: function () {
+        return $http({
+          method: 'get',
+          url: this.url() + "user/read/" + userService.getEmail()
+        });
+      },
+
+      logIn: function () {
+        return $http({
+          method: 'get',
+          url: this.url() + "user/logIn/" + userService.getEmail()
+        });
+      },
+
+
+
+      coefByCodeAndDate: function (date, code) {
+        return $http({
+          method: 'get',
+          url: this.url() + "currency/coef/" + code + "/" + this.dateUrl(date)
+        });
+      },
+
+      addCategory: function (category) {
+        return $http({
+          method: 'get',
+          url: this.url() + "user/addCategory/" + userService.getEmail() + "/" + category
+        });
+      },
+
+      itemById: function (id) {
+        return $http({
+          method: 'get',
+          url: this.url() + "item/readId/" + id
+        });
+      },
+
+
+
+    };
+
+
+  })
+
+
+
+
+
+
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'img/perry.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'img/mike.png'
+    }];
+
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
         }
+        return null;
       }
-      return null;
-    }
-  };
-});
+    };
+  });
